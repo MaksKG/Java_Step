99873e1ed8aef14ac1ef09f78f64ce7e619488b8
MaksKG <144100021+MaksKG@users.noreply.github.com>
2024-03-28 01:15:17 +0200

    1

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.HashSet;
-import.java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
